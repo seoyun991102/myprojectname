7581cc4d2dee05ce5dc13bb6ca80d02d9ae17dd3
sec <sec@LAPTOP-K268A31D>
2022-03-03 14:26:17 +0900

    first commit

---
+++ src/com/human/ex/javatest01.java
@@ -0,0 +1,19 @@
+package com.human.ex;
+
+public class javatest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		System.out.println("hi");
+	    System.out.println("hi");	
+	    
+	/*
+	 *프로그래밍 
+	 *완전어렵네 
+	 *와우시발
+	 */
+		
+	}
+}
+
---
+++ src/com/human/ex/javatest02.java
@@ -0,0 +1,20 @@
+package com.human.ex;
+
+public class javatest02 {
+
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub
+	System.out.println("일\t월\t화\t수\t목\t금\t토");
+	System.out.println("1\t2\t3\t4\t5\t6\t7");
+	System.out.println("8\t9\t10\t11\t12\t13\t14");
+	System.out.println("15\t16\t17\t18\t19\t20\t21");
+	System.out.println("22\t23\t24\t25\t26\t27\t28");
+	System.out.println("29\t30\t31");
+	}
+	
+}
+		   
+
+
+			 
+	  
